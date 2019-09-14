cfc80316a25678131893a896474dbab7d251e9f8
jerin17 <jerinthomas17@gamil.com>
2019-09-15 02:27:26 +0530

    Input and quotations

---
+++ 5.7InputAndQuotes.py
@@ -0,0 +1,23 @@
+greetings = "Hello "
+name = input("Please enter your name : ")
+print(greetings+name)
+
+splitString = "\nThis string \nhas been split \nover \nseveral times."
+print(splitString)
+
+tabbedString = "\n1\t2\t3\t4\t5"
+print(tabbedString)
+
+print("The pet shop owner said \"No, no, 'e's uh, . . . he's resting\"")
+print('The pet shop owner said \"No, no, \'e\'s uh, . . . he\'s resting\"')
+
+anotherSplitString='''
+This string 
+has also been split 
+over 
+several times.
+'''
+print(anotherSplitString)
+
+print('''The pet shop owner said "No, no, 'e's uh, . . . he's resting"''')
+print("""The pet shop owner said \"No, no, 'e's uh, . . . he's resting\"""")
