fea316e46b6a127adde6bceb6f60a0f244a19afe
jerin17 <jerinthomas17@gamil.com>
2019-09-15 02:09:28 +0530

    Python Basics

---
+++ 5.4PythonBasics.py
@@ -0,0 +1,11 @@
+print("Hello, World !")
+print(1 + 6)
+print(6 * 7)
+print()
+print("The End")
+print("Python's string are easy to use.")
+print('We can incude, "qoutes" in strings')
+print("Hello" + "World")
+greetings = "Hello "
+name = "Jerin"
+print(greetings+name)
