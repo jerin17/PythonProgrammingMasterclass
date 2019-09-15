5d7aa0af35fc6cff04c3f9f9e75a5beeb8e4013d
jerin17 <jerinthomas17@gamil.com>
2019-09-15 10:49:37 +0530

    Variables

---
+++ 5.9Variables.py
@@ -0,0 +1,50 @@
+greetings = "Hello"
+_name = "Jerin"
+abc = "Good "
+xyz = "morning"
+greet = "there"
+print(greetings + ' ' + greet)
+age=23
+print(age)
+
+# print(greetings+age)
+
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)    #divide, float value
+print(a // b)   #divide, int value
+print(a % b)    #remainder
+print(a + b / 3 - 4 * 12)
+print((((a + b)/3)-4)*12)
+print()
+
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[0])
+print(parrot[3])
+print(parrot[-1])
+print(parrot[2:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+
+number = "9, 123, 456, 789, 012, 345"
+print(number[1::5])
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9, 0"
+print(numbers[0::3])
+
+string = "Hello "
+print(string*5)
+
+today = "sunday"
+print("day" in today)
+print("sun" in today)
+print("Sun" in today)
+print("Fri" in today)
+print("abc" in "abcdefghij")
+print("abc" in "qwerty123")
